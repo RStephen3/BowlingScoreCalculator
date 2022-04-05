72c82ee88e6cff23a1af577d0875bbde3d29ed5f
Rachel Stephen <rachel2012@knights.ucf.edu>
2022-04-04 22:02:55 -0400

    Model push

---
+++ BowlingScoreCalculator/BowlingScoreCalculator/Models/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingLogic2021
+{
+    public class Ball
+    {
+        public Ball(int ballNumber)
+        {
+            Number = ballNumber;
+        }
+        public int Number { get; set; }
+        public int Value { get; set; }
+        public eMarkType MarkType { get; set; }
+    }
+}
---
+++ BowlingScoreCalculator/BowlingScoreCalculator/Models/Frame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingLogic2021
+{
+    public class Frame
+    {
+        public Frame() {}
+        public Frame(int frameNumber)
+        {
+            Number = frameNumber;
+            FirstBall = new Ball(1);
+            SecondBall = new Ball(2);
+            ThirdBall = new Ball(3);
+        }
+
+        public int Number { get; set; }
+        public Ball FirstBall { get; set; }
+        public Ball SecondBall { get; set; }
+        public Ball ThirdBall { get; set; }
+        public int Score { get; set; }
+        public eMarkType MarkType { get; set; }
+        public bool IsScoringComplete { get; set; }
+    }
+    public enum eMarkType
+    {
+        NotThrown,
+        Strike,
+        Spare,
+        Open
+    }
+}
